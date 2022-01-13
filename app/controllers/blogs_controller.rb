3890322bed87b0c76e8c41fd9d1c02203ef68f0b
fumtas1k <fumta01a7@gmail.com>
2022-01-13 23:42:07 +0900

    add:favorite機能追加

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,12 +2,14 @@ class BlogsController < ApplicationController
   before_action :set_blog, only: %i[show edit update destroy]
   def index
     @blogs = Blog.all.order(created_at: :desc)
+    @favorites = current_user.favorites
   end
   def new
     @blog = current_user.blogs.build
   end
   def confirm
     @blog = current_user.blogs.build(blog_params)
+    render :new if @blog.invalid?
   end
   def create
     @blog = current_user.blogs.build(blog_params)
@@ -23,11 +25,11 @@ class BlogsController < ApplicationController
   end
 
   def show
+    @favorite = current_user.favorites.find_by(blog_id: @blog.id)
   end
   def edit
   end
   def update
-
     if @blog.update(blog_params)
       redirect_to blog_path(@blog.id), notice: "blog編集しました!"
     else
@@ -35,12 +37,16 @@ class BlogsController < ApplicationController
     end
   end
   def destroy
+    @blog.destroy
+    flash[:danger] = "削除しました!"
+    redirect_to blogs_path
+
   end
   private
   def blog_params
     params.require(:blog).permit(:title, :content)
   end
   def set_blog
-    @blog = current_user.blogs.find(params[:id])
+    @blog = Blog.find(params[:id])
   end
 end
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,6 +1,11 @@
 class FavoritesController < ApplicationController
   def create
+    favorite = current_user.favorites.create(blog_id: params[:blog_id])
+    redirect_to request.referer, notice: "#{favorite.blog.user.name}の投稿をお気に入り登録しました"
   end
   def destroy
+    favorite = current_user.favorites.find_by(id: params[:id]).destroy
+    flash[:danger] = "#{favorite.blog.user.name}の投稿をお気に入り解除しました"
+    redirect_to request.referer
   end
 end
